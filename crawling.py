336766fe5a2cc6223f83942ef9032b05b8017581
dongdong21128 <100834827+dongdong21128@users.noreply.github.com>
2022-06-01 23:45:16 +0900

    Update crawling.py

--- crawling.py
+++ crawling.py
@@ -23,3 +23,13 @@ for movie in movies :
   k.append(movie.get_text('\n'))
 for i in range(1, 20) :
   print(i, k[i-1])
+#개봉일 출력 프로그램
+
+ticket=int(input("개봉일을 알고싶은 영화의 순위를 입력해주세요>>> "))
+
+day=soup.find_all("em", attrs={"class":"dday"})
+ps=soup.find_all("span", attrs={"class":"txt-info"})
+j=[]
+for p in ps :
+  j.append(p.get_text())
+print(j[ticket-1])
